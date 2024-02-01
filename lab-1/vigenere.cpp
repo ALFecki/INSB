d3fc535f34a3dc34effd881cfd6417869953e20c
Александр Скворцов <alexskvr03@gmail.com>
2024-02-01 22:37:49 +0300

    updated vigenere

--- lab-1/vigenere.cpp
+++ lab-1/vigenere.cpp
@@ -1,30 +1,43 @@
 #include <iostream>
 #include <fstream>
+#include <tuple>
 
-std::string encryptVigenere(std::string text, std::string keyword, bool encrypt = true) {
+std::tuple<char, int, int> getBaseParameters(std::string keyword, char textChar, int keywordIndex) {
+    char base = isupper(textChar) ? 'A' : 'a';
+    char offset = isupper(keyword[keywordIndex]) ? 'A' : 'a';
+    int messageIndex = textChar - base;
+    int keyCharIndex = keyword[keywordIndex] - offset;
+    return std::tuple<char, int, int>(base, messageIndex, keyCharIndex);
+}
+
+std::string encryptVigenere(std::string text, std::string keyword) {
     std::string result = "";
     int keywordIndex = 0;
-    for (int i = 0; i < text.length(); i++) {
-        if (isalpha(text[i])) {
-            char base = isupper(text[i]) ? 'A' : 'a';
-            char offset = isupper(keyword[keywordIndex]) ? 'A' : 'a';
-            int messageIndex = text[i] - base;
-            int keyCharIndex = keyword[keywordIndex] - offset;
-            if (encrypt) {
-                result += static_cast<char>((messageIndex + keyCharIndex) % 26 + base);
-            } else {
-                result += static_cast<char>((messageIndex - keyCharIndex + 26) % 26 + base);
-            }
+    for (auto& ch: text) {
+        if (isalpha(ch)) {
+            auto baseParameters = getBaseParameters(keyword, ch, keywordIndex);
+            result += static_cast<char>((std::get<1>(baseParameters) + std::get<2>(baseParameters)) % 26 + std::get<0>(baseParameters));
             keywordIndex = (keywordIndex + 1) % keyword.length();
         } else {
-            result += text[i];
+            result += ch;
         }
     }
     return result;
 }
 
 std::string decryptVigenere(std::string text, std::string keyword) {
-    return encryptVigenere(text, keyword, false);
+    std::string result = "";
+    int keywordIndex = 0;
+    for (auto& ch: text) {
+        if (isalpha(ch)) {
+            auto baseParameters = getBaseParameters(keyword, ch, keywordIndex);
+            result += static_cast<char>((std::get<1>(baseParameters) - std::get<2>(baseParameters) +26) % 26 + std::get<0>(baseParameters));
+            keywordIndex = (keywordIndex + 1) % keyword.length();
+        } else {
+            result += ch;
+        }
+    }
+    return result;
 }
 
 int main() {
