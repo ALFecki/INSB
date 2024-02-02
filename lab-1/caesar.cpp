1188dd6acc61a09f50c639dccb1d717499c3b9bb
Skvortsov A.V <alexskvr03@gmail.com>
2024-02-02 18:01:25 +0300

    caesar update

--- lab-1/caesar.cpp
+++ lab-1/caesar.cpp
@@ -1,7 +1,9 @@
-#include <iostream>
-#include <fstream>
-#include <string>
 #include <locale.h>
+
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <limits>
 
 std::string encryptCaesar(std::string text, int shift) {
     std::string result = "";
@@ -24,34 +26,56 @@ std::string decryptCaesar(std::string text, int shift) {
 }
 
 int main() {
+    std::string inputFileName = "input.txt", outputFileName = "output.txt", inputText;
 
-    std::string inputFileName, outputFileName, inputText;
+    std::cout << "CAESAR Cipher for alphabet \"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz\""
+              << std::endl;
 
-    std::cout << "CAESAR Cipher for alphabet \"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz\"" << std::endl;
-    std::cout << "Enter input file name: ";
-    std::getline(std::cin, inputFileName);
-    std::cout << "Enter output file name: ";
-    std::getline(std::cin, outputFileName);
+    std::cout << "Enter input file name (default: input.txt): ";
+    std::string temp;
+    std::getline(std::cin, temp);
+    if (!temp.empty()) {
+        inputFileName = temp;
+        temp.clear();
+    }
+    std::cout << "Enter output file name (default: output.txt): ";
+    std::getline(std::cin, temp);
+    if (!temp.empty()) {
+        outputFileName = temp;
+        temp.clear();
+    }
 
     std::ifstream inputFile(inputFileName);
     std::ofstream outputFile(outputFileName);
-    
-    if (inputFile.is_open()) {
-        std::getline(inputFile, inputText);
-        inputFile.close();
 
+    if (inputFile.is_open()) {
+        inputText =
+            std::string((std::istreambuf_iterator<char>(inputFile)), std::istreambuf_iterator<char>());
+        inputFile.close();
+    } else {
+        std::cout << "Unable to open the input file." << std::endl;
+        return 0;
+    }
+
+    while (true) {
         int shift = 3;
+        std::cout << "Enter shift: ";
+        if (!(std::cin >> shift)) {
+            std::cout << "Enter correct shift!" << std::endl;
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            continue;
+        }
 
         std::string encryptedText = encryptCaesar(inputText, shift);
         std::string decryptedText = decryptCaesar(encryptedText, shift);
 
-        outputFile << "Encrypted text: " << encryptedText << std::endl;
-        outputFile << "Decrypted text: " << decryptedText << std::endl;
+        outputFile << "Encrypted text:\n" << encryptedText << std::endl;
+        outputFile << "Decrypted text:\n" << decryptedText << std::endl;
 
         outputFile.close();
-    } else {
-        std::cout << "Unable to open the input file." << std::endl;
+        break;
     }
-    
+
     return 0;
 }
