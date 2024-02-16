bd3687432d6e89559a641ba02d6f5fa2654620a1
Skvortsov A.V <skvorcovav@oblgas.by>
2024-02-16 13:35:55 +0300

    updates for server

---
+++ lab-2/server.cpp
@@ -0,0 +1,14 @@
+#include "httplib.h"
+
+int main() {
+  using namespace httplib;
+
+  Server svr;
+
+  svr.Get("/hi", [](const Request& req, Response& res) {
+    res.set_content("Hello World!", "text/plain");
+  });
+
+  svr.listen("0.0.0.0", 8080);
+}
+
