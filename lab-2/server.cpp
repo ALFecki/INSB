136f4495fe41b65f18fa6e3e2f98b394a72f2edf
Skvortsov A.V <alexskvr03@gmail.com>
2024-02-22 15:58:51 +0300

    started server

--- lab-2/server.cpp
+++ lab-2/server.cpp
@@ -1,14 +1,41 @@
 #include "httplib.h"
 
-int main() {
-  using namespace httplib;
+struct ClientInfo {
+    std::string key;
+    std::string tgs;
+    std::string c_ss;
+    std::vector<std::string> allowed_servers;
+};
 
-  Server svr;
+struct ServerInfo {
+    std::string tgs;
+};
 
-  svr.Get("/hi", [](const Request& req, Response& res) {
-    res.set_content("Hello World!", "text/plain");
-  });
+std::map<std::string, ClientInfo> clients = {
+    {"alf", {.key = "test1", .tgs = "test2", .c_ss = "internal", .allowed_servers = {"testserver"}}}};
 
-  svr.listen("0.0.0.0", 8080);
+std::map<std::string, ServerInfo> servers = {{"testserver",
+                                              {
+                                                  .tgs = "serverkc",
+                                              }}};
+
+void initMessage(const httplib::Request& request, httplib::Response& response) {
+    std::string id = request.path_params.at("id");
+
+    if (clients.find(id) == clients.end()) {
+        response.status = 403;
+        response.set_content("Resource forbidden", 10, "text/plain");
+        return;
+    }
+
 }
 
+
+int main() {
+
+    Server svr;
+
+    svr.Get("/hi", [](const Request& req, Response& res) { res.set_content("Hello World!", "text/plain"); });
+
+    svr.listen("0.0.0.0", 8080);
+}
