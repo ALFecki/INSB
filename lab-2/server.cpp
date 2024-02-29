3b70f73f0508aa843dc01d714a52ca8f9a4d0e35
Skvortsov A.V <skvorcovav@oblgas.by>
2024-02-29 22:54:21 +0300

    server upd for new des

--- lab-2/server.cpp
+++ lab-2/server.cpp
@@ -1,8 +1,8 @@
-#include "httplib.h"
-#include "des/des.h"
+#include <httplib.h>
+#include <des.h>
+#include "utils.h"
 #include <iostream>
 #include <chrono>
-#include "utils.h"
 
 using namespace httplib;
 
@@ -17,9 +17,12 @@ int main() {
     c.setKey(key);
 
     svc.Post("/hello", [&](const Request& request, Response& response) {
+        cout << "POST body from client to SS: " << request.body << endl;
+
         auto tgs = split(c.decryptAnyString(split(request.body, ';').front()), ';');
 
         string c_ss = tgs.back();
+        cout << "C_SS from client: " << c_ss << endl;
         string client_id = tgs.front();
 
         cout << "Received tgs from client '" << client_id << "': " << endl;
@@ -32,12 +35,15 @@ int main() {
 
         auto aut = split(c.decryptAnyString(split(request.body, ';').back()), ';');
 
-        cout << "Received aut from client '" << client_id << "': " << endl;
+        cout << "Received aut from client: " << endl;
 
         for (const auto& cur : aut) {
+            cout << "!!!" << endl;
             cout << cur << endl;
         }
-
+        std::cout << client_id << endl;
+        cout << string(aut.front()) << endl;
+        
         if (client_id != string(aut.front())) {
             response.status = 403;
             const char* resp = "Forbidden (incorrect user)";
