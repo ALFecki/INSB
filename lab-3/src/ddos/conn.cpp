8c4f35d54e4a2916cf1329e7242323c8abd7e754
Skvortsov A.V <skvorcovav@oblgas.by>
2024-03-21 01:23:43 +0300

    connection class

---
+++ lab-3/src/ddos/conn.cpp
@@ -0,0 +1,17 @@
+#include "conn.h"
+
+Connection::Connection(boost::asio::io_context& io_context) : sock(io_context) {
+	buffer.prepare(1024);
+}
+
+auto Connection::create(boost::asio::io_context& io_context) {
+	return std::shared_ptr<Connection>(new Connection(io_context));
+}
+
+ip::tcp::socket& Connection::socket() {
+	return sock;
+}
+
+void Connection::printMessage(const std::string& msg) {
+	std::cout << msg << std::endl;
+}
---
+++ lab-3/src/ddos/conn.h
@@ -0,0 +1,23 @@
+#include <boost/asio.hpp>
+#include <boost/asio/io_context.hpp>
+#include <iostream>
+
+using namespace boost::asio;
+using namespace boost::asio::ip;
+
+using namespace boost::asio;
+
+class Connection : public std::enable_shared_from_this<Connection> {
+public:
+	ip::tcp::socket& socket();
+
+	static auto create(boost::asio::io_context&);
+
+private:
+	Connection(boost::asio::io_context&);
+
+	void printMessage(const std::string&);
+
+	ip::tcp::socket sock;
+	streambuf buffer;
+};
